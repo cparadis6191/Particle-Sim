e069c72c4e580060e5720fb57526d728c93d74f5
Chad Paradis <cparadis6191@gmail.com>
2012-07-05 20:32:14 -0400

    Added some new source files and graphics

--- slid.cpp
+++ slid.cpp
@@ -6,6 +6,19 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Surface * const screen = SDL_SetVideoMode(640, 480, 8, SDL_SWSURFACE);
+
+	SDL_Surface * const sprite = SDL_LoadBMP("./graphics/duck.bmp");
+
+	SDL_BlitSurface(sprite, 0, screen, 0);
+	SDL_Flip(screen);
+
+	while(1)
+	SDL_Delay(5);
+
+	SDL_Quit();
 
 	return 0;
 }
